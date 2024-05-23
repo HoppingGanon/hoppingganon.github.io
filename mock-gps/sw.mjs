f942fe83054bcb0ca13416162fe35fbb4ad28fcb
HoppingGanon <HoppingGanonApp@gmail.com>
2024-05-23 23:38:50 +0900

    [add] sw追加

--- mock-gps/sw.mjs
+++ mock-gps/sw.mjs
@@ -11,74 +11,20 @@ const urlsToCache = [
   "./offline.html",
 ];
 
-const CACHE_KEYS = [CACHE_NAME];
-
 self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches
-      .open(CACHE_NAME) // 上記で指定しているキャッシュ名
-      .then(function (cache) {
-        return cache.addAll(urlsToCache); // 指定したリソースをキャッシュへ追加
-        // 1つでも失敗したらService Workerのインストールはスキップされる
-      })
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener("fetch", function (event) {
-  //ブラウザが回線に接続しているかをboolで返してくれる
-  var online = navigator.onLine;
-
-  //回線が使えるときの処理
-  if (online) {
-    event.respondWith(
-      caches.match(event.request).then(function (response) {
-        if (response) {
-          return response;
-        }
-        //ローカルにキャッシュがあればすぐ返して終わりですが、
-        //無かった場合はここで新しく取得します
-        return fetch(event.request)
-          .then(function (response) {
-            // 取得できたリソースは表示にも使うが、キャッシュにも追加しておきます
-            // ただし、Responseはストリームなのでキャッシュのために使用してしまうと、ブラウザの表示で不具合が起こる(っぽい)ので、複製しましょう
-            cloneResponse = response.clone();
-            if (response) {
-              //ここ&&に修正するかもです
-              if (response || response.status == 200) {
-                //現行のキャッシュに追加
-                caches.open(CACHE_NAME).then(function (cache) {
-                  cache.put(event.request, cloneResponse).then(function () {
-                    //正常にキャッシュ追加できたときの処理(必要であれば)
-                  });
-                });
-              } else {
-                //正常に取得できなかったときにハンドリングしてもよい
-                return response;
-              }
-              return response;
-            }
-          })
-          .catch(function (error) {
-            //デバッグ用
-            return console.log(error);
-          });
-      })
-    );
-  } else {
-    //オフラインのときの制御
-    event.respondWith(
-      caches.match(event.request).then(function (response) {
-        // キャッシュがあったのでそのレスポンスを返す
-        if (response) {
-          return response;
-        }
-        //オフラインでキャッシュもなかったパターン
-        return caches.match("./offline.html").then(function (responseNodata) {
-          //適当な変数にオフラインのときに渡すリソースを入れて返却
-          //今回はoffline.htmlを返しています
-          return responseNodata;
-        });
-      })
-    );
-  }
+  event.respondWith(
+    fetch(event.request).catch(function () {
+      return caches.match(event.request).then(function (response) {
+        return response || caches.match("./offline.html");
+      });
+    })
+  );
 });
